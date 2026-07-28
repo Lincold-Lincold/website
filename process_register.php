4bfa3206419b20e593d61e173f775af085168c1c
Vapor <jasperlincoldpflores@gmail.com>
2026-07-28 15:47:56 +0800

    Revised some files

--- process_register.php
+++ process_register.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registation Results</title>
+</head>
+<body>
+    <h2 style="display: flex; justify-content: center; font-family:Arial;">Registration Result Pages</h2>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
 <?php
     $firstName = $_POST['fName'] ??'';
     $lastName = $_POST['lName'] ??'';
